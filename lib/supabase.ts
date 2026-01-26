64d2cd7cfacfa846dd2e24c6d99ec69abb35269a
Modestas Kumpys <mode@Modestass-MacBook-Air.local>
2026-01-26 10:10:42 +0100

    chore: Switch Supabase backend to Athplan(EU)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://haicfgsgimpwnukympab.supabase.co'
-const supabaseKey = 'sb_publishable_TlM38PD842aKFHGgUvmrdQ_wWrdhweW'
+const supabaseUrl = 'https://ppjzhesecvagtwfbvoek.supabase.co'
+const supabaseKey = 'sb_publishable_CuhiPd8Q8xUXc1l44MddOA_wdLoR9c4'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
